09c0f20a20b70c20614acad4fa9c7f1ed6be2269
ChakeGetSome <koreshkovasiliy@gmail.com>
2018-12-06 18:10:58 +0300

    1.3 JavaDoc. Code convention [#88584]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,13 @@
 package ru.job4j.calculate;
+/**
+* Calculate
+* @author Vasiliy Koreshkov 
+*/
 public class Calculate {
+	/**
+	* Main
+	* @return Hello World
+	*/
  public static void main(String[] args) {
   System.out.println("Hello World");
  }
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Vasiliy Koreshkov
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
