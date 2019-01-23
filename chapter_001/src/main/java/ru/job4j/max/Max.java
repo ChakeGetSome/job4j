797d324a7b9d5d015aee7a1650ae731d861f776c
ChakeGetSome <koreshkovasiliy@gmail.com>
2019-01-23 18:44:06 +0300

    4.2. Максимум из двух чисел[#88549]

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,18 @@
+package ru.job4j.max;
+
+/**
+ * @version $Id$
+ * @since 0.1
+ */
+public class Max {
+    /**
+     * @author Koreshkov Vasiliy
+     * Возваращает большее число.
+     * @return большее число.
+     */
+    public int max(int first, int second){
+
+
+        return first > second ? first : second ;
+    }
+}
